60f5f2db3bc5bc2fadf54768253c0a9f2c38faee
Alessandro <info@a2area.it>
2022-06-23 08:33:23 +0200

    Added default installation for software

---
+++ _install/software.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+ECHO This script require scoop (https://scoop.sh/)
+ECHO ---------------------------------------------------
+PAUSE
+ECHO This script require execution on Windows PowerSheel
+ECHO ---------------------------------------------------
+PAUSE
+ECHO This script require add extra bucket for Scoop
+ECHO Install with command: scoop bucket add extras
+ECHO ---------------------------------------------------
+PAUSE
+ECHO 
+
+:: VS Code
+scoop install vscode
